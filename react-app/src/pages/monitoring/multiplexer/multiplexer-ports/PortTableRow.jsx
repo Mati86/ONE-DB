33498886095dc4e22b500420933dafaf048f4524
sohjpeg <sbukhari.bscs23seecs@student.nust.edu.pk>
2025-06-25 18:44:27 +0500

    fixed repetitive configure and moniter buttons

--- react-app/src/pages/monitoring/multiplexer/multiplexer-ports/PortTableRow.jsx
+++ react-app/src/pages/monitoring/multiplexer/multiplexer-ports/PortTableRow.jsx
@@ -1,11 +1,15 @@
 import { Box, Button, TableCell, TableRow } from '@mui/material';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import useApiPoll from '../../../../hooks/useApiPoll';
 import { OPTICAL_PORT_PARAMS, PORT_TYPE } from '../../../../utils/data';
 import { getReadApiPayloadForPort } from '../../../../utils/utils';
 
 function PortTableRow({ portNumber, portType }) {
+  const location = useLocation();
+  const isConfigurationRoute = location.pathname.startsWith('/configuration');
+  const isMonitoringRoute = location.pathname.startsWith('/monitoring');
+  
   const data = useApiPoll(
     10000,
     getReadApiPayloadForPort(portNumber, [
@@ -32,31 +36,36 @@ function PortTableRow({ portNumber, portType }) {
         )}
         {portType === PORT_TYPE.Demultiplexer && (
           <TableCell>{data?.data[OPTICAL_PORT_PARAMS.OutputPower]}</TableCell>
-        )}
-
-        <TableCell>
+        )}        <TableCell>
           <Box display='flex'>
-            <Link
-              to={`/monitoring/optical-ports/${portNumber}`}
-              style={{ textDecoration: 'none' }}
-            >
-              <Button size='small' variant='contained'>
-                Monitor
-              </Button>
-            </Link>
-            <Link
-              to={`/configuration/optical-ports/${portNumber}`}
-              style={{ textDecoration: 'none' }}
-            >
-              <Button
-                size='small'
-                variant='contained'
-                color='secondary'
-                sx={{ ml: 1 }}
+            {/* Show Monitor button only in monitoring routes */}
+            {isMonitoringRoute && (
+              <Link
+                to={`/monitoring/optical-ports/${portNumber}`}
+                style={{ textDecoration: 'none' }}
               >
-                Configure
-              </Button>
-            </Link>
+                <Button size='small' variant='contained'>
+                  Monitor
+                </Button>
+              </Link>
+            )}
+            
+            {/* Show Configure button only in configuration routes */}
+            {isConfigurationRoute && (
+              <Link
+                to={`/configuration/optical-ports/${portNumber}`}
+                style={{ textDecoration: 'none' }}
+              >
+                <Button
+                  size='small'
+                  variant='contained'
+                  color='secondary'
+                  sx={{ ml: isMonitoringRoute ? 1 : 0 }}
+                >
+                  Configure
+                </Button>
+              </Link>
+            )}
           </Box>
         </TableCell>
       </TableRow>
